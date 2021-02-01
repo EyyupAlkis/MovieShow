75834fe8c793373b029e9a0591b4babb064347e3
Eyyüp Alkış <alkis.eyyup@gmail.com>
2021-02-01 17:02:26 +0300

    delete comment

--- app/src/main/java/dev/alks/movieshow/data/datasource/model/MovieDetailResponse.kt
+++ app/src/main/java/dev/alks/movieshow/data/datasource/model/MovieDetailResponse.kt
@@ -4,20 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 
 data class Episode(
-  /**  val id: Int,
-    val url: String,
-    val name: String,
-    val season: Int,
-    val number: Int,
-    val type: String,
-    val airdate: String,
-    val airtime: String,
-    val airstamp: String,
-    val runtime: Int,
-    val image: Image,
-    val summary: String,
-    @SerializedName("_links") val links: Link,**/
-
     @SerializedName("id") val id : Int,
     @SerializedName("url") val url : String,
     @SerializedName("name") val name : String,
